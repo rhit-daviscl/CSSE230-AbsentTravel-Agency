6cb00c9c8537384551a4ff0e625052baff21653e
daviscl <daviscl@rose-hulman.edu>
2021-02-10 17:30:26 -0500

    Removed UnKnown Graph Class and main unsued class

--- main.java
+++
@@ -1,6 +0,0 @@
-public main(){
-  //This is the main class
-}
-//This is a comment by andrew walkowski
-// this is another comment by andrew
-//This is a comment by Curtiss Davis
--- src/Graph.java
+++
@@ -1,8 +0,0 @@
-
-public class GraphExample{
-	public static void main(String[] args) {
-		List<Integer> l = new LinkedList<>();
-		l.add(10);
-		System.out.println(l);
-	}
-}
