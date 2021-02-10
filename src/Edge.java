aceec0b669f4d668fb5a96f9054785e38d1293c8
jneill775 <45371750+jneill775@users.noreply.github.com>
2021-02-10 12:33:22 -0500

    Add files via upload

---
+++ src/Edge.java
@@ -0,0 +1,11 @@
+
+public class Edge {
+	
+	
+	public int getTime(Node n) {
+		return n.timeCost;
+	}
+	public int getDistance(Node n) {
+		return n.distanceCost;
+	}
+}
---
+++ src/Node.java
@@ -0,0 +1,15 @@
+
+public class Node {
+	public String name;
+	public int distanceCost;
+	public int timeCost;
+	
+	public Node(String s) {
+		this.name = s;
+	}
+	
+	public void setName(String newName) {
+		this.name = newName;
+	}
+	
+}
