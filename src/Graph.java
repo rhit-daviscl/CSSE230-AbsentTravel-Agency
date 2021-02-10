293ea55735f125e502c12697a096841370428538
walkowae <walkowae@rose-hulman.edu>
2021-02-10 12:34:41 -0500

    TravelingTree Project created

--- src/Graph.java
+++ src/Graph.java
@@ -1,4 +1,8 @@
 
-public class Graph {
-
+public class GraphExample{
+	public static void main(String[] args) {
+		List<Integer> l = new LinkedList<>();
+		l.add(10);
+		System.out.println(l);
+	}
 }
